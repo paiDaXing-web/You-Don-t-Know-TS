7fc790f6d1a9b91768833b27baffb7868081b3ae
maxueming <maxueming@kuaishou.com>
2022-11-08 12:01:45 +0800

    feat: 新增网站

---
+++ vuepress/.vuepress/config.ts
@@ -0,0 +1,107 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "You-Dont-Know-TS",
+  description: "Just playing around",
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "reco_luan",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "Home", link: "/" },
+      { text: "Categories", link: "/categories/reco/1/" },
+      { text: "Tags", link: "/tags/tag1/1/" },
+      {
+        text: "Docs",
+        children: [
+          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+        ],
+      },
+    ],
+    bulletin: {
+      body: [
+        {
+          type: "text",
+          content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "QQ 群",
+        },
+        {
+          type: "text",
+          content: `
+          <ul>
+            <li>QQ群1：1037296104</li>
+            <li>QQ群2：1061561395</li>
+            <li>QQ群3：962687802</li>
+          </ul>`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "GitHub",
+        },
+        {
+          type: "text",
+          content: `
+          <ul>
+            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+          </ul>`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "buttongroup",
+          children: [
+            {
+              text: "打赏",
+              link: "/docs/others/donate.html",
+            },
+          ],
+        },
+      ],
+    },
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  }),
+  // debug: true,
+});
---
+++ vuepress/README.md
@@ -0,0 +1,60 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - MdContent
+  - Footer
+bannerBrand:
+  heroImage: /logo.png
+  heroImageStyle:
+    maxWidth: "300px"
+    width: "100%"
+    display: block
+    margin: "0 auto 1rem"
+    borderRadius: "1rem"
+  bgImage: "/bg.svg"
+  heroText: You-Don't-Know-TypeScript
+  tagline: 这是一个专注研究Typescript的网站，在这里你可以全面深入学习Typescript相关知识，你还可以挑战相应的题目，快来学习吧。
+  buttons:
+    - { text: 我要学习, link: "/docs/guide/introduce" }
+    - {
+        text: 我要挑战,
+        link: "/docs/style-default-api/introduce",
+        type: "plain",
+      }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+features:
+  - title: 过去
+    details: 开发一款看着开心、写着顺手的 vuepress 博客主题。
+  - title: 当下
+    details: 帮助更多的朋友节省时间去用心书写内容，而不是仅仅配置一个博客去孤芳自赏。
+  - title: 未来
+    details: 吸引更多的朋友参与到开发中来，继续强大功能。
+---
+
+## 快速开始
+
+**npx**
+
+```bash
+# 初始化，并选择 2.x
+npx @vuepress-reco/theme-cli init
+```
+
+**npm**
+
+```bash
+# 初始化，并选择 2.x
+npm install @vuepress-reco/theme-cli@1.0.7 -g
+theme-cli init
+```
+
+**yarn**
+
+```bash
+# 初始化，并选择 2.x
+yarn global add @vuepress-reco/theme-cli@1.0.7
+theme-cli init
+```
